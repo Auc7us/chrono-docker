b0f5e17ea1fdd0cf65de9e349b8bd551c897afaf
auc7us <keshavsharan97@gmail.com>
2025-01-30 16:04:26 -0600

    add chrono build in mount drive script

--- setup_chrono_docker.sh
+++ setup_chrono_docker.sh
@@ -9,9 +9,13 @@ echo "Extracting chrono-docker.zip..."
 unzip chrono-docker.zip
 
 echo "Changing directory to chrono-docker..."
-cd chrono-docker
+cd chrono-docker/mountdir
+git clone -b 9.0.1 https://github.com/projectchrono/chrono.git
+
+cd ..
 
 echo "Running atk development setup..."
 sudo atk dev -dbua -s chrono -o x11 gpus
 
-echo "Setup complete."
+echo "Setup complete; Run buildChronoInMount.sh located at ~/mountDrive inside the container
+ to build Chrono again in a persistent mount directory on host"
