16ed4fa899a0fb5329234c9bb28a444fded49171
auc7us <keshavsharan97@gmail.com>
2025-02-10 11:49:09 -0600

    swtich to single persistent chrono build

--- setup_chrono_docker.sh
+++ setup_chrono_docker.sh
@@ -5,17 +5,17 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-echo "Extracting chrono-docker.zip..."
+echo "Extracting chrono-orb.zip..."
 unzip chrono-docker.zip
 
 echo "Changing directory to chrono-docker..."
 cd chrono-docker/mountdir
-git clone -b 9.0.1 https://github.com/projectchrono/chrono.git
+git clone -b 9.0.1 https://github.com/Auc7us/chrono.git
 
 cd ..
 
 echo "Running atk development setup..."
 sudo atk dev -dbua -s chrono -o x11 gpus
 
-echo "Setup complete; Run buildChronoInMount.sh located at ~/mountDrive inside the container
- to build Chrono again in a persistent mount directory on host"
+echo "Setup complete; Please run buildChronoInMount.sh located at ~/mountDrive inside the container
+ to build Chrono in a persistent mount directory on host"
